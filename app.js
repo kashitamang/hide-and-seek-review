5b99222c74b9fd1add34a502292d296b05a5e5f9
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-09 10:24:53 -0800

    adds handleGuess function

--- app.js
+++ app.js
@@ -37,9 +37,21 @@ boulderButton.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
     // then increment the guesses
+    totalGuesses++;
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
+    // console.log(correctSpot);
+    document.getElementById(`${correctSpot}-container`).classList.add('face');
     // then if the user guess is correct, increment the correct guesses
+    if (correctSpot === userGuess) {
+        correctGuesses++;
+    }
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+    totalEl.textContent = totalGuesses;
+    winsEl.textContent = correctGuesses;
+    lossesEl.textContent = totalGuesses - correctGuesses;
 }
